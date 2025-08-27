05cecbf7fff334e87b707d0797b9428d27566c84
TaweesakSangkoranee <taweesaksaelim2409@gmail.com>
2025-08-27 19:20:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('envelope').addEventListener('click', () => {
+  document.getElementById('envelope').style.display = 'none';
+  document.getElementById('card').classList.remove('hidden');
+});
